33852eb0f711023edc6a237a771795b5eddde6d4
Jared C. Gober <jcgober@Jareds-Air.lan1>
2020-09-07 20:11:46 -0400

    Selecting view allows you to view selection in console.

--- index.js
+++ index.js
@@ -45,7 +45,129 @@ inquirer
         ]
         }).then(function(answers){
 
-            console.log(answers.viewing)
+            if(answers.viewing === "Department"){
+
+
+                var mysql = require("mysql");
+
+                var connection = mysql.createConnection({
+                    host: "localhost",
+                  
+                    // Your port; if not 3306
+                    port: 3306,
+                  
+                    // Your username
+                    user: "root",
+                  
+                    // Your password
+                    password: "rootroot",
+                  
+                    database: "employee_tracker_db"
+                  });
+                
+                  connection.connect(function(err) {
+                    if (err) throw err;
+                    console.log("connected as id " + connection.threadId);
+                    afterConnection();
+                });
+                
+                function afterConnection() {
+                    connection.query("SELECT * FROM department", function(err, res) {
+                        if (err) throw err;
+                        console.table(res)
+                        connection.end();
+                    });
+                }
+
+                
+
+
+
+
+
+            }else if (answers.viewing === "Role"){
+
+                var mysql = require("mysql");
+
+                var connection = mysql.createConnection({
+                    host: "localhost",
+                  
+                    // Your port; if not 3306
+                    port: 3306,
+                  
+                    // Your username
+                    user: "root",
+                  
+                    // Your password
+                    password: "rootroot",
+                  
+                    database: "employee_tracker_db"
+                  });
+                
+                  connection.connect(function(err) {
+                    if (err) throw err;
+                    console.log("connected as id " + connection.threadId);
+                    afterConnection();
+                });
+                
+                function afterConnection() {
+                    connection.query("SELECT * FROM role", function(err, res) {
+                        if (err) throw err;
+                        console.table(res)
+                        connection.end();
+                    });
+                }
+
+
+
+
+
+            }else if (answers.viewing === "Employee"){
+
+
+
+                var mysql = require("mysql");
+
+                var connection = mysql.createConnection({
+                    host: "localhost",
+                  
+                    // Your port; if not 3306
+                    port: 3306,
+                  
+                    // Your username
+                    user: "root",
+                  
+                    // Your password
+                    password: "rootroot",
+                  
+                    database: "employee_tracker_db"
+                  });
+                
+                  connection.connect(function(err) {
+                    if (err) throw err;
+                    console.log("connected as id " + connection.threadId);
+                    afterConnection();
+                });
+                
+                function afterConnection() {
+                    connection.query("SELECT * FROM employee", function(err, res) {
+                        if (err) throw err;
+                        console.table(res)
+                        connection.end();
+                    });
+                }
+
+
+
+
+
+            }
+
+
+
+
+
+    
         }); 
 
     } else if (answers.action === "Update"){
